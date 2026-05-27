dc521abff3d6745339fd12fc2558792467b3abc3
tomalbrc <tomalbrc@me.com>
2026-05-27 17:01:22 +0200

    back to just gradle

--- src/main/java/de/tomalbrc/itemsfloat/FloatingItemSystem.java
+++ src/main/java/de/tomalbrc/itemsfloat/FloatingItemSystem.java
@@ -10,12 +10,14 @@ import com.hypixel.hytale.component.dependency.SystemDependency;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
 import com.hypixel.hytale.math.util.ChunkUtil;
-import com.hypixel.hytale.protocol.ChangeVelocityType;
 import com.hypixel.hytale.server.core.modules.entity.component.BoundingBox;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.modules.entity.item.ItemComponent;
+import com.hypixel.hytale.server.core.modules.entity.item.ItemPhysicsComponent;
 import com.hypixel.hytale.server.core.modules.entity.item.ItemPhysicsSystem;
+import com.hypixel.hytale.server.core.modules.entity.item.ItemPrePhysicsSystem;
 import com.hypixel.hytale.server.core.modules.physics.component.Velocity;
+import com.hypixel.hytale.server.core.modules.projectile.system.StandardPhysicsTickSystem;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.chunk.ChunkColumn;
 import com.hypixel.hytale.server.core.universe.world.chunk.section.FluidSection;
@@ -66,38 +68,59 @@ public class FloatingItemSystem extends EntityTickingSystem<EntityStore> {
 
     @Override
     public void tick(float dt, int index, @Nonnull ArchetypeChunk<EntityStore> chunk, @Nonnull Store<EntityStore> store, @Nonnull CommandBuffer<EntityStore> buffer) {
+        ItemPhysicsComponent itemComponent = chunk.getComponent(index, ItemPhysicsComponent.getComponentType());
         TransformComponent transform = chunk.getComponent(index, TransformComponent.getComponentType());
-        if (transform != null) {
-            Vector3d pos = transform.getPosition();
-            double unfilteredY = pos.y;
-            int x = (int) Math.floor(pos.x);
-            int y = (int) Math.floor(unfilteredY);
-            int z = (int) Math.floor(pos.z);
+        if (transform == null) return;
 
-            var fluidLevel = this.isInFluid(store, x, y, z);
-            var blockAboveIsFluid = this.isInFluid(store, x, y + 1, z);
+        Vector3d pos = transform.getPosition();
+        int x = (int) Math.floor(pos.x);
+        int y = (int) Math.floor(pos.y);
+        int z = (int) Math.floor(pos.z);
 
-            if (fluidLevel > 0) {
-                double maxFluidLevel = 8.0;
+        int fluidLevel = this.isInFluid(store, x, y, z);
+        int fluidAbove = this.isInFluid(store, x, y + 1, z);
+        if (fluidLevel == 0 && fluidAbove == 0) return;
 
-                double fluidHeight = blockAboveIsFluid > 0 ? 1.0 : (fluidLevel / maxFluidLevel);
+        Velocity velocity = chunk.getComponent(index, Velocity.getComponentType());
+        if (velocity == null) return;
 
-                double v = fluidHeight - ((pos.y + 0.4) - (double) y);
-
-                var velocity = chunk.getComponent(index, Velocity.getComponentType());
-                if (velocity != null && (blockAboveIsFluid > 0 || v > 0)) {
-                    var s = 1.1;
-
-                    Vector3d jumpVector = new Vector3d(0, blockAboveIsFluid > 0 ? s : s * v, 0).add(velocity.getVelocity().get(new Vector3d()).mul(0.5));
-                    velocity.addInstruction(jumpVector, null, ChangeVelocityType.Set);
-                }
-            }
+        double waterSurface;
+        if (fluidAbove > 0) {
+            waterSurface = 2.0;
+        } else if (fluidLevel > 0) {
+            waterSurface = 1.5;
+        } else {
+            return;
         }
+
+        double depth = waterSurface - (pos.y - y);
+        if (depth <= 0) return;
+
+        Vector3d currentVel = velocity.getVelocity().get(new Vector3d());
+
+        double strength = 30;
+        double upwardAccel = strength * depth;
+
+        double damping = 3;
+        double dampingAccel = -damping * currentVel.y;
+
+        double netDeltaVy = (upwardAccel + dampingAccel) * dt;
+
+        velocity.set(currentVel.mul(0.98).add(0, netDeltaVy, 0));
+    }
+
+    @Override
+    public boolean isParallel(int archetypeChunkSize, int taskCount) {
+        return false;
     }
 
     @Nonnull
     @Override
     public Set<Dependency<EntityStore>> getDependencies() {
-        return Set.of(new SystemDependency<>(Order.BEFORE, ItemPhysicsSystem.class));
+        return Set.of(
+                new SystemDependency<>(Order.AFTER, StandardPhysicsTickSystem.class),
+                new SystemDependency<>(Order.BEFORE, ItemPrePhysicsSystem.class),
+                new SystemDependency<>(Order.AFTER, ItemPhysicsSystem.class)
+        );
     }
 }
